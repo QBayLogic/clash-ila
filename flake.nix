f21703539cb88d4503fedf1486b25c0a3a02ff2a
jaschutte <34577095+jaschutte@users.noreply.github.com>
2025-02-28 16:09:15 +0100

    Updated nix

--- flake.nix
+++ flake.nix
@@ -7,25 +7,24 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    clash-compiler.url = "github:clash-lang/clash-compiler/dd/nix-update";
+    clash-compiler.url = "github:clash-lang/clash-compiler";
     ecpprog.url = "github:diegodiv/ecpprog";
   };
-  outputs = { self, nixpkgs, ecpprog, flake-utils, clash-compiler, ... }:
+  outputs = { nixpkgs, ecpprog, flake-utils, clash-compiler, ... }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = clash-compiler.inputs.nixpkgs.legacyPackages.${system};
-          overlay = final: prev: {
-            clash-cores = clash-compiler.packages.${system}.clash-cores;
-            clash-prelude = clash-compiler.packages.${system}.clash-prelude;
-            clash-lib = clash-compiler.packages.${system}.clash-lib;
-            clash-ghc = clash-compiler.packages.${system}.clash-ghc;
-            cabal-install = nixpkgs.legacyPackages.${system}.cabal-install;
-          };
-          # TODO: refer dynamically to the right ghc version (using clash-compiler's default).
-          # Might require some changes on clash-compiler's flake.
-          myHsPkgs = (pkgs.haskell.packages.ghc910.extend overlay).extend
-            (pkgs.haskell.lib.compose.packageSourceOverrides {
-              orangecrabby = ./orangecrab;
-            });
+      let
+        overlay = final: prev: {
+          clash-cores = clash-compiler.packages.${system}.clash-cores;
+          clash-prelude = clash-compiler.packages.${system}.clash-prelude;
+          clash-lib = clash-compiler.packages.${system}.clash-lib;
+          clash-ghc = clash-compiler.packages.${system}.clash-ghc;
+          cabal-install = nixpkgs.legacyPackages.${system}.cabal-install;
+        };
+        pkgs = clash-compiler.inputs.nixpkgs.legacyPackages.${system};
+        myHsPkgs = (pkgs.haskell.packages.ghc910.extend overlay).extend
+          (pkgs.haskell.lib.compose.packageSourceOverrides {
+            orangecrabby = ./orangecrab;
+          });
       in
       {
         devShells.default = myHsPkgs.shellFor {
@@ -35,22 +34,21 @@
             clash-compiler.packages.${system}.clash-ghc.env
           ];
           nativeBuildInputs = 
-            with pkgs; [
+            [
               # For interacting with the OrangeCrab board.
-              yosys trellis nextpnr
-              gnumake
-              # To have some nice HTML to publish for Coq proofs.
-              python311Packages.alectryon
+              pkgs.yosys
+              pkgs.trellis
+              pkgs.nextpnr
+              pkgs.gnumake
 
-              haskell-language-server
-            ] ++
-            (with pkgs.coqPackages; [
-              coq mathcomp hierarchy-builder mathcomp-word serapi
-            ])
-            ++
-            (with myHsPkgs; [ cabal-install haskell-language-server ])
-            ++
-            [ ecpprog.defaultPackage.${system} ]
+              # Haskell stuff
+              myHsPkgs.cabal-install
+              myHsPkgs.haskell-language-server
+              myHsPkgs.clash-ghc
+
+              # Idk
+              ecpprog.defaultPackage.${system}
+            ]
           ;
         };
       });
