33d0b6c3e962505c8b65a903cdd8e90b7d176995
jaschutte <34577095+jaschutte@users.noreply.github.com>
2025-03-05 11:31:01 +0100

    Fix test clarity
    After weekly discussion, some small reworks to make the testing code
    slightly more clear

--- orangecrab/tests/Tests/RingBuffer.hs
+++ orangecrab/tests/Tests/RingBuffer.hs
@@ -11,26 +11,23 @@ import qualified Data.Maybe as DM
 import RingBuffer
 
 -- | Define the start/middle/end of a sequence, making sequences testing easier
-data SequenceState a = SSIgnore | SSValue a | SSEnd deriving(Generic, NFDataX)
+data SequenceState a = SSNothing | SSValue a | SSInputEnd deriving(Generic, NFDataX)
 intoMaybe :: SequenceState a -> Maybe a
-intoMaybe SSIgnore = Nothing
-intoMaybe SSEnd = Nothing
+intoMaybe SSNothing = Nothing
+intoMaybe SSInputEnd = Nothing
 intoMaybe (SSValue a) = Just a
 
 intoSS :: Maybe a -> SequenceState a
-intoSS Nothing = SSIgnore
+intoSS Nothing = SSNothing
 intoSS (Just a) = SSValue a
 
 isEndSS :: SequenceState a -> Bool
-isEndSS SSEnd = True
+isEndSS SSInputEnd = True
 isEndSS _ = False
 
 -- | Simulate feeding input into the ring buffer
 simulateDump ::
-  forall dom a size . (KnownDomain dom, NFDataX a, KnownNat size, 1 <= size) =>
-  Clock dom ->
-  Reset dom ->
-  Enable dom ->
+  forall dom a size . (HiddenClockResetEnable dom, NFDataX a, KnownNat size, 1 <= size) =>
   -- | Buffer size
   SNat size ->
   -- | Init value for the buffer
@@ -39,25 +36,25 @@ simulateDump ::
   [Maybe a] ->
   -- | Buffer content
   [a]
-simulateDump clk rst en size ini rawInput = go
+simulateDump size ini rawInput = go
   where
     -- Pad our input data so we can capture every output signal and ignore the reset clock
     inputList = DL.concat [
-        [SSIgnore], -- Reset clock
+        [SSNothing], -- Reset clock
         intoSS <$> rawInput, -- Our input
-        [SSEnd], -- Trigger the read
-        DL.replicate (fromInteger $ snatToInteger size) SSIgnore -- Pad the signal so we can read it all
+        [SSInputEnd], -- Trigger the read
+        DL.replicate (fromInteger $ snatToInteger size) SSNothing -- Pad out the signal so we can read the input back
       ]
     input = fromList inputList
-    ring = (withClockResetEnable clk rst en ringBuffer) size ini (pure False) $ (intoMaybe <$> input)
-    ringOutput = (withClockResetEnable clk rst en dumpRingBuffer) (isEndSS <$> input) ring
+    ring = ringBuffer size ini (pure False) (intoMaybe <$> input)
+    ringOutput = dumpRingBuffer (isEndSS <$> input) ring
     go = DM.catMaybes $ sampleN (DL.length inputList + 1) ringOutput
 
 -- | Expected output of the ring buffer
 -- There are two cases to consider;
 --  1. Our input is smaller than or equal to the buffer
 --  2. Our input is bigger than the buffer
--- In case 1, the expected output is the same as the input, with the initial values filling in the rest
+-- In case 1, the expected output is the same as the input
 -- In case 2, the expected output are the last inserted values
 expectedDump ::
   -- | Buffer size
@@ -86,5 +83,9 @@ writeProperty = property $ do
   initValue <- forAll $ Gen.int (Range.constant 1000 2000)
   randomInput <- forAll $ Gen.list (Range.linear 0 300) $ Gen.maybe $ Gen.int (Range.constant 0 100)
 
-  simulateDump systemClockGen systemResetGen enableGen d50 initValue randomInput === expectedDump d50 randomInput
+  let
+    actual = withClockResetEnable @System clockGen resetGen enableGen simulateDump d50 initValue randomInput
+    expected = expectedDump d50 randomInput
+
+  actual === expected
 
