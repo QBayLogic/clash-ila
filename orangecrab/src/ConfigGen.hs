03b5b9e076b04523538525c7dffd846ac3b5d5c0
jaschutte <34577095+jaschutte@users.noreply.github.com>
2025-03-28 09:27:32 +0100

    Add IlaConfiguration
    Generates a JSON file on disk and passes a `IlaConfig` record to the end
    user. Allowing the ILA system to know how it's configured without having
    to waste storage for that on the ILA itself.

--- orangecrab/src/ConfigGen.hs
+++ orangecrab/src/ConfigGen.hs
@@ -1,124 +1,91 @@
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE OverloadedRecordDot #-}
 {-# LANGUAGE OverloadedRecordUpdate #-}
-{-# LANGUAGE RebindableSyntax #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TemplateHaskellQuotes #-}
 {-# LANGUAGE NoFieldSelectors #-}
 
 module ConfigGen where
 
-import Clash.Prelude
+import Clash.Prelude hiding (Exp)
 
 import Data.Aeson
 
-import GHC.Records (getField)
-
 import Language.Haskell.TH
-import Language.Haskell.TH.Syntax
 
-import Data.Data (Proxy)
+import Data.HList
 import Data.Hashable
-import Data.List
-import Language.Haskell.TH.Datatype
-import Control.Monad
 
-sig1 :: Signal dom (BitVector 12)
-sig1 = pure 8
+-- | A datakind enforcing a HList to be convertable to a list of tuples, with size and the label of each signal
+class NamedSignal ts where
+  getSizeAndName :: HList ts -> [(Int, String)]
 
-sig2 :: (KnownNat n) => Signal dom (Index n)
-sig2 = pure 2
+instance NamedSignal '[] where
+  getSizeAndName HNil = []
 
-sig3 :: Signal System (Index 420)
-sig3 = pure 69
-
-sigs ::
-  (KnownNat n) => SNat n -> Signal dom (BitVector 12, Index n)
-sigs _ = bundle (sig1, sig2)
-
-sigk :: Signal dom (BitVector 12, Index 5)
-sigk = bundle (sig1, sig2)
-
-getBitSize :: Name -> Q Language.Haskell.TH.Type
-getBitSize name = do
-  ForallT _ _ (AppT _ typeInfo) <- reifyType name
-
-  bitsized <- [t| BitSize $(return typeInfo) |]
-
-  expandSyns bitsized
-
-woo :: Name -> Q Language.Haskell.TH.Exp
-woo name = do
-  [| sizeFromSignal $(varE name) |]
-
-signalNameSize :: [Name] -> Q Language.Haskell.TH.Exp
-signalNameSize names = [| $(listE $ Data.List.map (\name -> [| (name, woo $(varE name)) |] ) names) |]
-
-
--- fileContent :: (HiddenClockResetEnable dom, Lift a) => Signal dom a
--- fileContent sig = [| $(Clash.Prelude.lift sig) |]
-
--- getBitSize :: Name -> Q Language.Haskell.TH.Exp
-
-printBitSize :: forall n. (KnownNat n) => Proxy n -> String
-printBitSize i = show $ natVal @n i
-
-sizeFromSignal ::
-  forall dom a n.
-  (BitPack a, n ~ BitSize a) =>
-  Signal dom a -> Int
-sizeFromSignal _ = natToNum @n
-
-class GenConfig dom a b t where
-  genConfig :: (BitPack a, Lift a, BitPack b, Lift b) => [GenSignal] -> (Signal dom a) -> (Signal dom b, String) -> Q t
-
--- last input
-instance GenConfig dom a b (SNat n -> String -> Q (Signal dom (b,a))) where
-  genConfig acc sacc (x, name_x) n topLeveLName = do
-        let genSignals = nacc
-        let nonHashed = GenIla topLeveLName (snatToNum n) 0 genSignals
-        let hashedIla = GenIla topLeveLName (snatToNum n) (hash nonHashed) genSignals
-        let genIlas = GenIlas [hashedIla]
-        runIO $ encodeFile "ilaconf.json" genIlas
-        return $ bundle (x, sacc)
-    where
-      nacc = acc Data.List.++ [(GenSignal name_x (sizeFromSignal x))] 
-
-instance forall dom t a b. (GenConfig t dom a b, BitPack a, Lift a) => GenConfig ((Signal dom a, String) -> (Signal dom (a,t))) dom a (Signal dom b) where
-  genConfig acc sacc (x, name_x) =
-    genConfig (acc Data.List.++ [GenSignal name_x (sizeFromSignal @dom x)]) (bundle (x, sacc))
+instance forall dom t ts. (BitPack t, NamedSignal ts) => NamedSignal ((Signal dom t, String) : ts) where
+  getSizeAndName (HCons (_, label) xs) = (natToNum @(BitSize t), label) : getSizeAndName xs
 
 writeConfig ::
-  forall dom n a .
-  (KnownNat n, BitPack a) => SNat n -> String -> Unbundled dom a -> [String] -> Q Language.Haskell.TH.Syntax.Exp
-writeConfig _ toplevelName signals names = do
-  -- let sized = sizeFromSignal <$> signals
-  -- let sized = signalListSizes signals
-  -- let names = signalListNames signals
+  forall dom a n ts.
+  ( KnownNat n
+  , NamedSignal ts
+  , Lift a
+  ) =>
+  -- | How many samples should it capture of each signal?
+  SNat n ->
+  -- | Merely an identifier, recommended to be the name of the toplevel design, but it can be any name you fancy
+  String ->
+  -- | A list of signals and labels (Strings) tupled together to sample
+  HList ts ->
+  -- | The same list of signals, but then bundled together
+  Signal dom a ->
+  -- | a `IlaConfig` in AST form, this should be passed directly to the ILA
+  Q Exp
+writeConfig size toplevelName namedSignals bundledSignals = do
+  let sizeNames = getSizeAndName namedSignals
 
-  -- let genSignals = (uncurry GenSignal) <$> (Data.List.zip names sized)
-  let genSignals = (\n -> GenSignal n 1) <$> names
+  let genSignals = (uncurry $ flip GenSignal) <$> sizeNames
 
   let nonHashed = GenIla toplevelName (natToNum @n) 0 genSignals
-  let hashedIla = GenIla toplevelName (natToNum @n) (hash nonHashed) genSignals
+  let hashValue = hash nonHashed
+  let hashValueBv = resize $ pack hashValue :: BitVector 32
+  let hashedIla = GenIla toplevelName (natToNum @n) hashValue genSignals
 
   let genIlas = GenIlas [hashedIla]
 
   runIO (encodeFile "ilaconf.json" genIlas)
 
-  [|str|]
+  let ilaConf =
+        IlaConfig
+          { hash = hashValueBv
+          , size = size
+          , tracing = bundledSignals
+          }
 
-data IlaConfig a = IlaConfig
+  [|ilaConf|]
+
+-- A record containing the actual configuration of the ILA
+data IlaConfig n a = IlaConfig
   { hash :: BitVector 32
+  -- ^ A hash to verify which ILA the system is communicating with
+  , size :: SNat n
+  -- ^ Size of the buffers, aka; how many samples should it capture
   , tracing :: a
+  -- ^ The signal to trace
   }
+  deriving (Lift)
 
+-- All types primarily just used to properly format a JSON document
+
+-- | Individual signal JSON representation
 data GenSignal = GenSignal
   { name :: String
   , width :: Int
   }
   deriving (Generic, Show, ToJSON, Eq, Hashable)
 
+-- | Individual ILA JSON representation
 data GenIla = GenIla
   { toplevel :: String
   , bufferSize :: Int
@@ -127,6 +94,7 @@ data GenIla = GenIla
   }
   deriving (Generic, Show, ToJSON, Eq, Hashable)
 
+-- | Toplevel of the JSON file
 data GenIlas = GenIlas
   { ilas :: [GenIla]
   }
