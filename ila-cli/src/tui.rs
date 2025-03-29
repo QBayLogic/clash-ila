9286dd039c303ad05cba03ab3c3d281edea853a9
jaschutte <34577095+jaschutte@users.noreply.github.com>
2025-03-29 16:51:47 +0100

    Add cursor bound logic
    Handles logic around cursor boundaries when typing in text

--- ila-cli/src/tui.rs
+++ ila-cli/src/tui.rs
@@ -3,10 +3,7 @@ use std::{
     time::Duration,
 };
 
-use crossterm::{
-    cursor::MoveTo,
-    event::{Event as TuiEvent, KeyModifiers},
-};
+use crossterm::event::{Event as TuiEvent, KeyModifiers};
 use crossterm::{
     event::{poll, read, KeyCode},
     execute,
@@ -14,20 +11,60 @@ use crossterm::{
 };
 use tui::{backend::CrosstermBackend, layout::Rect, style::Style, widgets::*, *};
 
+// TODO: make it a stateful widget, where the state is the cursor
+// This is because you need to know the size it renders at to know when to make the cursor move
+
 #[derive(Debug, PartialEq, Eq, Clone)]
-struct TextPrompt {
+struct TextPromptState {
     title: String,
     input: String,
     cursor: usize,
+    cursor_offset: usize,
     visible: usize,
+    render_bounds: (u16, u16),
 }
 
-impl Widget for TextPrompt {
-    fn render(self, area: layout::Rect, buf: &mut buffer::Buffer) {
-        let limit: String = self
+impl TextPromptState {
+    fn calculate_visible(&mut self, area: Rect) {
+        self.visible = self
+            .input
+            .len()
+            .saturating_sub(area.width as usize)
+            .saturating_sub(self.cursor_offset);
+    }
+
+    fn left(&mut self) {
+        self.cursor = self.cursor.saturating_sub(1);
+        if self.cursor != 0 && self.get_cursor_x() == self.render_bounds.0 {
+            self.cursor_offset = (self.cursor_offset + 1).min(self.input.len())
+        }
+    }
+
+    fn right(&mut self) {
+        self.cursor = self.input.len().min(self.cursor + 1);
+        if self.cursor != self.input.len() && self.get_cursor_x() == self.render_bounds.1 {
+            self.cursor_offset = self.cursor_offset.saturating_sub(1);
+        }
+    }
+
+    fn get_cursor_x(&self) -> u16 {
+        (self.render_bounds.0 + self.cursor.saturating_sub(self.visible) as u16)
+            .min(self.render_bounds.1)
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+struct TextPrompt;
+
+impl StatefulWidget for TextPrompt {
+    type State = TextPromptState;
+
+    fn render(self, area: layout::Rect, buf: &mut buffer::Buffer, state: &mut Self::State) {
+        let limit: String = state
             .input
             .chars()
-            .skip(self.input.len().saturating_sub(area.width as usize))
+            .skip(state.visible)
+            .take(area.width as usize)
             .collect();
         buf.set_string(area.x, area.y, limit, Style::default());
     }
@@ -36,7 +73,7 @@ impl Widget for TextPrompt {
 #[derive(Debug, PartialEq, Eq, Clone)]
 enum TuiState {
     Main,
-    InPrompt(TextPrompt),
+    InPrompt(TextPromptState),
 }
 
 pub struct TuiSession {
@@ -60,10 +97,10 @@ impl TuiSession {
     pub fn render(&mut self) {
         let _ = self.term.draw(|f| {
             let size = f.size();
-            let title_block = Block::default().title("ILA").borders(Borders::ALL);
+            let title_block = Block::default().title("ILA - Port ").borders(Borders::ALL);
             f.render_widget(title_block, size);
 
-            if let TuiState::InPrompt(text_prompt) = self.state.clone() {
+            if let TuiState::InPrompt(text_prompt) = &mut self.state {
                 let width = size.width.saturating_sub(10);
                 let around = Rect::new(
                     size.x + size.width.div_ceil(2).saturating_sub(width / 2),
@@ -83,13 +120,12 @@ impl TuiSession {
                     around.height.saturating_sub(2),
                 );
 
-                f.set_cursor(
-                    (center.x + text_prompt.cursor.saturating_sub(text_prompt.visible) as u16)
-                        .min(size.width),
-                    center.y,
-                );
+                text_prompt.calculate_visible(center);
+                text_prompt.render_bounds.0 = center.x;
+                text_prompt.render_bounds.1 = center.x + center.width;
 
-                f.render_widget(text_prompt, center);
+                f.set_cursor(text_prompt.get_cursor_x(), center.y);
+                f.render_stateful_widget(TextPrompt, center, text_prompt);
             }
         });
     }
@@ -117,23 +153,25 @@ impl TuiSession {
                         break;
                     }
                     (TuiState::Main, KeyCode::Char('v'), _) => {
-                        let default = "dump.vcd";
-                        self.state = TuiState::InPrompt(TextPrompt {
+                        let default = "dump.vcd-abcdefghijklmnopqrstuvwxyz1234567890";
+                        self.state = TuiState::InPrompt(TextPromptState {
                             title: format!("Save VCD file (default: {default})"),
                             input: default.to_string(),
                             cursor: default.len(),
                             visible: 0,
+                            cursor_offset: 0,
+                            render_bounds: (0, u16::MAX),
                         })
                     }
                     (TuiState::InPrompt(text_prompt), KeyCode::Char(c), _) => {
                         text_prompt.input.insert(text_prompt.cursor, c);
-                        text_prompt.cursor += 1;
+                        text_prompt.right();
                     }
                     (TuiState::InPrompt(text_prompt), KeyCode::Backspace, _) => {
                         if text_prompt.cursor != 0 && text_prompt.cursor <= text_prompt.input.len()
                         {
                             text_prompt.input.remove(text_prompt.cursor - 1);
-                            text_prompt.cursor = text_prompt.cursor.saturating_sub(1);
+                            text_prompt.left();
                         }
                     }
                     (TuiState::InPrompt(text_prompt), KeyCode::Delete, _) => {
@@ -144,10 +182,10 @@ impl TuiSession {
                         }
                     }
                     (TuiState::InPrompt(text_prompt), KeyCode::Left, _) => {
-                        text_prompt.cursor = text_prompt.cursor.saturating_sub(1);
+                        text_prompt.left();
                     }
                     (TuiState::InPrompt(text_prompt), KeyCode::Right, _) => {
-                        text_prompt.cursor = text_prompt.input.len().min(text_prompt.cursor + 1);
+                        text_prompt.right();
                     }
                     _ => (),
                 }
