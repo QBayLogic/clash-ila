1efcec256c375817d451cb5381daee23e90c4ba4
jaschutte <34577095+jaschutte@users.noreply.github.com>
2025-04-18 17:01:12 +0200

    Added etherboneDfPacketizer

--- orangecrab/src/Communication.hs
+++ orangecrab/src/Communication.hs
@@ -1,12 +1,18 @@
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE OverloadedRecordDot #-}
+{-# LANGUAGE NoFieldSelectors #-}
+
 module Communication where
 
 import Clash.Cores.UART (ValidBaud, uart)
 import Clash.Prelude
 import Data.Maybe qualified as DM
-import Data.Type.Equality ((:~:) (Refl))
 import Protocols
 import Protocols.Df qualified as Df
 import Protocols.PacketStream
+
+import Debug.Trace
+import Data.String.Interpolate (__i)
 
 {- | Send out `Df` data through UART
 
@@ -140,6 +146,169 @@ df2ps = Circuit exposeIn
       , toPs <$> incoming
       )
 
+{- | Searches for a statically known 'preamble'
+It returns the same stream of bytes as came in, but with a flag indicating if a preamble has
+been found. Only the last element of the preamble will have the flag set true.
+
+Example:
+
+>>> let wcre = withClockResetEnable systemClockGen systemResetGen enableGen
+>>> let input = [NoData, NoData, Data 1, NoData, Data 1, Data 2, Data 3, NoData, NoData, Data 5]
+>>> take 11 $ simulateC (wcre (findPreamble (1 :> 2 :> 3 :> Nil))) def { resetCycles = 1 } input
+[NoData,NoData,NoData,Data (1,False),NoData,Data (1,False),Data (2,False),Data (3,True),NoData,NoData]
+-}
+findPreamble ::
+  forall dom n a.
+  ( HiddenClockResetEnable dom
+  , KnownNat n
+  , Eq a
+  ) =>
+  -- | The preamble to search for
+  Vec n a ->
+  -- | The circuit to slot in
+  Circuit
+    (Df dom a)
+    (Df dom (a, Bool))
+findPreamble preamble = Circuit exposeIn
+ where
+  exposeIn (fwd, bwd) = out
+   where
+    transfer :: Index n -> Df.Data a -> (Index n, Df.Data (a, Bool))
+    transfer _ Df.NoData = (0, Df.NoData)
+    transfer i (Df.Data a)
+      | i == maxBound && a == preamble !! i = (0, Df.Data (a, True))
+      | a == preamble !! i = (i + 1, Df.Data (a, False))
+      | otherwise = (0, Df.Data (a, False))
+
+    out = (bwd, mealy transfer 0 fwd)
+
+-- | The current parsing state of `etherboneDfPacketizer`
+data DepacketizeDfState
+  = EBNone
+  -- ^ Currently not parsing / searching for the magic
+  | EBHeader (Index 1)
+  -- ^ Parsing the EBHeader
+
+  -- | EBPadding (Index 1)
+  | EBRecord (Index 2)
+  -- ^ Parse the header of an EBRecord, this is the field containing how many read/writes there are
+  | EBBody (Index 514, Index 2)
+  -- ^ The value for wCound and rCount are 8 bit, meaning there are at most 256 entries (for each).
+  -- There's also the base address fields, so that makes the total 256 * 2 + 2 = 514
+  --
+  -- The second index is for 32 and 64 bit mode, where the entry may take up multiple 16 bit words
+  -- In our case, we only support 32 bit etherbone, so it's always `Index 2`
+  deriving (Generic, NFDataX, Show)
+
+-- | On a Df input, packetize the data such that it can be fed into `etherboneC` without trouble.
+--
+-- The problem this function intends to solve is that for certain kinds of mediums, the complete
+-- packet length isn't known ahead of time. Making the construction of the `PacketStream` difficult.
+--
+-- This function allows one to have a stream of bytes and it will attempt to format it into a
+-- `PacketStream`, so it can be fed to `etherboneC`. 
+--
+-- # Limitations
+--
+-- For now, this function __ONLY__ supports 32 bit etherbone packets. 64 bit etherbone packets will
+-- get treated as 32 bit, which __WILL__ cause you problems if you try it. So don't :)
+--
+-- Another limitation is that due to the nature of etherbone, it is impossible for us to determine
+-- how many EBRecords there are. This function assumes each etherbone packet only contains
+-- __one__ EBRecord.
+etherboneDfPacketizer ::
+  forall dom.
+  (HiddenClockResetEnable dom) =>
+  Circuit
+    (Df dom (BitVector 8))
+    (PacketStream dom 4 ())
+-- etherboneDfPacketizer = upConverterC @2 @2 <| Circuit exposeIn <| Df.compressor Nothing toWord
+etherboneDfPacketizer = upConverterC @2 @2 <| Circuit exposeIn <| Df.compressor Nothing toWord
+ where
+  toWord ::
+    (Maybe (BitVector 8)) -> BitVector 8 -> (Maybe (BitVector 8), Maybe (Vec 2 (BitVector 8)))
+  toWord Nothing m = (Just m, Nothing)
+  toWord (Just m) l = (Nothing, Just $ m :> l :> Nil)
+
+  exposeIn (fwd, bwd) = out
+   where
+    -- | Parse WCount and RCount fields in an EBRecord and return how long the expected packet is
+    --
+    -- We *2 is done due to us assuming we're using 32 bit etherbone, so each address/value is
+    -- 32 bits. Each field is 16 bits.
+    --
+    -- We should add +1, as there are 16 bits taken
+    getLength :: Vec 2 (BitVector 8) -> Index 514
+    getLength word = case word of
+      (0 :> 0 :> Nil) -> 0
+      (wCount :> 0 :> Nil) -> unpack $ resize wCount + 1
+      (0 :> rCount :> Nil) -> unpack $ resize rCount + 1
+      (wCount :> rCount :> Nil) -> (unpack $ resize rCount) + (unpack $ resize wCount) + 1
+      _ -> 0 -- Haskell is stupid
+
+    -- W 3 (1 * 2 - 1)
+    -- W 2
+    -- W 1
+    -- W 0
+    -- END
+
+    -- | Create the PS packet without `_last`
+    partialPs word =
+      Just $
+        PacketStreamM2S
+          { _last = Nothing
+          , _abort = False
+          , _meta = ()
+          , _data = word
+          }
+    -- | Create the PS packet with `_last`
+    completePS word =
+      Just $
+        PacketStreamM2S
+          { _last = Just maxBound
+          , _abort = False
+          , _meta = ()
+          , _data = word
+          }
+
+    transfer' ::
+      DepacketizeDfState ->
+      Df.Data (Vec 2 (BitVector 8)) ->
+      (DepacketizeDfState, Maybe (PacketStreamM2S 2 ()))
+    transfer' EBNone (Df.Data word@(0x4e :> 0x6f :> Nil)) = (EBHeader maxBound, partialPs word)
+    transfer' EBNone _ = (EBNone, Nothing) -- Silently drop packets if they're not part of etherbone
+    transfer' (EBHeader n) (Df.Data word)
+      | n == 0 = (EBRecord maxBound, partialPs word)
+      | otherwise = (EBHeader (n - 1), partialPs word)
+    -- transfer (EBPadding n) (Df.Data word)
+    --   | n == 0 = (EBRecord maxBound, partialPs word)
+    --   | otherwise = (EBPadding (n - 1), partialPs word)
+    transfer' (EBRecord n) (Df.Data word)
+      | n == 0 && getLength word == 0 = (EBNone, completePS word)
+      -- | n == 0 = (EBBody $ (trace [__i|len #{getLength word}|] $ getLength word), partialPs word)
+      | n == 0 = (EBBody (getLength word, maxBound), partialPs word)
+      | otherwise = (EBRecord (n - 1), partialPs word)
+    transfer' (EBBody (n, w)) (Df.Data word)
+      | n == 0 && w == 0 = (EBNone, completePS word)
+      | w == 0 = (EBBody (n - 1, maxBound), partialPs word)
+      | otherwise = (EBBody (n, w - 1), partialPs word)
+    -- When we receive no data, keep the current state
+    transfer' state Df.NoData = (state, Nothing)
+
+    transfer ::
+      DepacketizeDfState ->
+      (PacketStreamS2M, Df.Data (Vec 2 (BitVector 8))) ->
+      (DepacketizeDfState, Maybe (PacketStreamM2S 2 ()))
+    transfer state (bwdData, fwdData)
+      -- | _ready bwdData = trace [__i|#{state} with #{fwdData} -> #{transfer' state fwdData}|] $ transfer' state fwdData
+      | _ready bwdData = transfer' state fwdData
+      | otherwise = (state, Nothing)
+  
+    outwardsData :: Signal dom (Maybe (PacketStreamM2S 2 ()))
+    outwardsData = mealy transfer EBNone (bundle (bwd, fwd))
+
+    out = (Ack . DM.isJust <$> outwardsData, outwardsData)
+
 {- | Given a stream of bytes, replace the `_last` with `Nothing`, every `n`th packet will have
 `Just 1` set. This is useful in combination with `upConverterC`
 -}
@@ -158,32 +327,11 @@ mergeBytePackets _ = Circuit exposeIn
  where
   exposeIn (fwdIn, bwdIn) = out
    where
-    counter :: Signal dom (Index n)
-    counter = register 0 $ mux (DM.isJust <$> fwdIn) (satAdd SatWrap 1 <$> counter) counter
+    transfer ::
+      Index n -> Maybe (PacketStreamM2S 1 a) -> (Index n, Maybe (PacketStreamM2S 1 a))
+    transfer _ Nothing = (0, Nothing)
+    transfer i (Just packet)
+      | i == natToNum @(n - 1) = (0, Just packet{_last = Just 1})
+      | otherwise = (i + 1, Just packet{_last = Nothing})
 
-    replaceLast (Just packet) count
-      | count == natToNum @(n - 1) = Just packet{_last = Just 1}
-      | otherwise = Just packet{_last = Nothing}
-    replaceLast Nothing _ = Nothing
-
-    out = (bwdIn, liftA2 replaceLast fwdIn counter)
-
--- | Converts a `Signal dom (Maybe a)` into a `Df dom a`. Since CSignals do not accept backpressure
--- all backpressure will be ignored and is hence, unsafe.
-unsafeCSignalToDf ::
-  forall dom a.
-  (HiddenClockResetEnable dom) =>
-  Circuit
-    (CSignal dom (Maybe a))
-    (Df dom a)
-unsafeCSignalToDf = Circuit exposeIn
- where
-  exposeIn (incoming, _) = out
-   where
-    toData Nothing = Df.NoData
-    toData (Just a) = Df.Data a
-
-    out =
-      ( pure ()
-      , toData <$> incoming
-      )
+    out = (bwdIn, mealy transfer 0 fwdIn)
