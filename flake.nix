207d0120d77381dfb3f868493c06334a712f99d8
jaschutte <34577095+jaschutte@users.noreply.github.com>
2025-06-05 16:22:32 +0200

    Add flake build dependencies
    Rather than using cabal.project, use the flakes to manage dependencies

--- flake.nix
+++ flake.nix
@@ -9,18 +9,62 @@
   outputs = { nixpkgs, ecpprog, flake-utils, clash-compiler, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
+        non-flake-srcs = {
+          clash-protocols = pkgs.fetchFromGitHub {
+            owner = "clash-lang";
+            repo = "clash-protocols";
+            rev = "2ef336c2a0ca969cc5c8ad9cdb54cca5bd47f0bf";
+            hash = "sha256-sJX78AELxzV+y8GvqcLGvE1fWreWZMkhL18fdxsomjA=";
+          };
+          clash-cores = pkgs.fetchFromGitHub {
+            owner = "clash-lang";
+            repo = "clash-cores";
+            rev = "34fb54f80a89205640cdf1fdab678139493e097e";
+            hash = "sha256-ZDxLDBLqlZ4OCORPb7pnT3Ini5LoZlKN58HoWFgeeWw=";
+          };
+          circuit-notation = pkgs.fetchFromGitHub {
+            owner = "cchalmers";
+            repo = "circuit-notation";
+            rev = "564769c52aa05b90f81bbc898b7af7087d96613d";
+            hash = "sha256-sPfLRjuMxqVRMzXrHRCuKKrdTdqgAJ33pf11DoTP84Q=";
+          };
+        };
         overlay = final: prev: {
-          clash-cores = clash-compiler.packages.${system}.clash-cores;
+          cabal-install = nixpkgs.legacyPackages.${system}.cabal-install;
           clash-prelude = clash-compiler.packages.${system}.clash-prelude;
+          clash-prelude-hedgehog = clash-compiler.packages.${system}.clash-prelude-hedgehog;
           clash-lib = clash-compiler.packages.${system}.clash-lib;
           clash-ghc = clash-compiler.packages.${system}.clash-ghc;
-          cabal-install = nixpkgs.legacyPackages.${system}.cabal-install;
+
+          string-interpolate = clash-input-pkgs.haskell.lib.doJailbreak (prev.string-interpolate);
+
+          circuit-notation = final.developPackage {
+            root = non-flake-srcs.circuit-notation.outPath;
+            overrides = overlay;
+          };
+          clash-protocols-base = final.developPackage {
+            root = "${non-flake-srcs.clash-protocols.outPath}/clash-protocols-base";
+            overrides = overlay;
+          };
+          clash-protocols = final.developPackage {
+            root = "${non-flake-srcs.clash-protocols.outPath}/clash-protocols";
+            overrides = overlay;
+          };
+          clash-cores = final.developPackage {
+            root = non-flake-srcs.clash-cores.outPath;
+            overrides = overlay;
+          };
+          clash-ila = hs-pkgs.developPackage {
+            root = ./ila;
+            overrides = overlay;
+          };
         };
-        clashPkgs = clash-compiler.inputs.nixpkgs.legacyPackages.${system};
-        hsPkgs = clashPkgs.haskell.packages.ghc910.extend overlay;
+        clash-input-pkgs = clash-compiler.inputs.nixpkgs.legacyPackages.${system};
+        hs-pkgs = clash-input-pkgs.haskell.packages.ghc910.extend overlay;
         pkgs = nixpkgs.legacyPackages.${system};
       in
       {
+        hs = hs-pkgs;
         devShells.default = pkgs.mkShell {
           inputsFrom = [
             clash-compiler.packages.${system}.clash-lib.env
@@ -36,8 +80,8 @@
               pkgs.gnumake
 
               # Haskell stuff
-              hsPkgs.cabal-install
-              hsPkgs.haskell-language-server
+              hs-pkgs.cabal-install
+              hs-pkgs.haskell-language-server
 
               # Rust subproject
               pkgs.cargo
@@ -50,10 +94,11 @@
               # Surfer to view VCD
               pkgs.surfer
 
-              # Idk
+              # Program the FPGA
               ecpprog.defaultPackage.${system}
             ]
           ;
         };
+        packages.default = hs-pkgs.clash-ila;
       });
 }
