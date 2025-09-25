4bdd7c4798fd94b90b7a9be7bb6cdd2fbfa1f2a5
GrootProgrammer <ryan.groot+github@gmail.com>
2025-09-25 11:57:37 +0200

    fix the introduced dom type application

--- ila/examples/ClashIlaDemo.hs
+++ ila/examples/ClashIlaDemo.hs
@@ -2,7 +2,6 @@
 {-# LANGUAGE OverloadedRecordDot #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE NoFieldSelectors #-}
-{-# LANGUAGE TypeApplications #-}
 {-# OPTIONS_GHC -fconstraint-solver-iterations=10 #-}
 {-# OPTIONS_GHC -fplugin=Protocols.Plugin #-}
 
@@ -52,7 +51,7 @@ topLogicUart baud rx = tx
         -- ^ The name displayed in the waveform viewer
         , triggerPoint = 0
         -- ^ Amount of samples in the buffer after trigger
-        , predicates = (ilaDefaultPredicates @dom)
+        , predicates = ilaDefaultPredicates
         -- ^ The list of predicates to select from during runtime
         }
   tx = snd $ demoIla (rx, (pure ()))
